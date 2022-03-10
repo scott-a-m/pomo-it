094ff0ccee18255e83215974fffc1f4c16e84618
Scott Mitchell <scott.a.mitchell1@hotmail.com>
2022-03-10 09:16:22 +0800

    added 404 error page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import FormHeader from "../components/FormHeader";
+import { Link } from "react-router-dom";
+
+const NotFound = ({ userData, setUserData }) => {
+  return (
+    <div>
+      <FormHeader userData={userData} setUserData={setUserData} />
+      <div className="form-wrapper">
+        <div className="form-box">
+          <h1 className="">404 Page Not Found</h1>
+          <p>
+            This page does not exist. Back to <Link to="/">homepage</Link>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
